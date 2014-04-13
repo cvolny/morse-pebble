dba946711446e3e477c8d08ecc916cbd95f60e52
Christopher J. Volny <chris@volnybook.thevolny.net>
2014-04-13 08:53:46 -0500

    updated url

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var MORSE_JSON_URL = "https://doc-0c-6c-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/tbbjaj3fgp2co0t3voces3n61hbv6vtp/1397361600000/13528935449689788747/*/0B9faOy_iuEqCWGF5Y1owNFozem8?h=16653014193614665626&e=downloadhttps://doc-0c-6c-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/tbbjaj3fgp2co0t3voces3n61hbv6vtp/1397361600000/13528935449689788747/*/0B9faOy_iuEqCWGF5Y1owNFozem8?h=16653014193614665626&e=download";
+var MORSE_JSON_URL = "https://raw.githubusercontent.com/cvolny/morse-pebble/master/morse.json";
 
 var morse_lookup_table = {};
 var message = "";
